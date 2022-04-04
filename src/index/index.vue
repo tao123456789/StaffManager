fd34392e7e685c480d5276c3c8c205eb9b387f26
Administrator <2413629661@qq.com>
2022-04-04 16:18:25 +0800

    2022-04-02:首页的卡片使用栅栏响应移动端

--- src/index/index.vue
+++ src/index/index.vue
@@ -1,59 +1,65 @@
 <template>
   <div>
     <el-container style="padding-left: 20px;padding-top: 40px;">
-      <div style="padding-right: 20px">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span style="font-size: 30px">个人应用系统</span>
-            <el-button style="float: right;width: 150px" type="primary" @click="enterMyTools">点击进入</el-button>
-          </div>
-          <el-form style="background-image: url('../../static/img/img3.jpg');background-size: cover;height: 200px">
-            <el-form-item label="说明：" ></el-form-item>
-            <el-form-item>用于个人应用开发</el-form-item>
-            <el-form-item></el-form-item>
-          </el-form>
-        </el-card>
-      </div>
-      <div>
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span style="font-size: 30px">后台管理系统</span>
-            <el-button style="float: right;width: 150px" type="primary" @click="enterMMS">点击进入</el-button>
-          </div>
-          <el-form style="background-image: url('../../static/img/img2.png');background-size: cover;height: 200px">
-            <el-form-item label="2022-02-07  更新说明：" ></el-form-item>
-            <el-form-item style="font-size: 200%">1，加入首页的卡片界面</el-form-item>
-            <el-form-item>1，更新首页信息</el-form-item>
-          </el-form>
-        </el-card>
-      </div>
-      <div style="padding-right: 20px">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span style="font-size: 30px">前端组件Demo库</span>
-            <el-button style="float: right;width: 150px" type="primary" @click="enterDemo">点击进入</el-button>
-          </div>
-          <el-form style="background-image: url('../../static/img/img4.png');background-size: cover;height: 200px">
-            <el-form-item label="说明：" ></el-form-item>
-            <el-form-item>用于记录前端的组件应用</el-form-item>
-            <el-form-item></el-form-item>
-          </el-form>
-        </el-card>
-      </div>
-      <div style="padding-right: 20px">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span style="font-size: 30px">开发中......</span>
-            <el-button style="float: right;width: 150px" type="primary" @click="enterTest">点击进入</el-button>
-          </div>
-          <el-form style="background-image: url('../../static/img/img1.jpg');background-size: cover;height: 200px">
-            <el-form-item label="说明：" ></el-form-item>
-            <el-form-item>通过路由控制系统入口，进行转发</el-form-item>
-            <el-form-item></el-form-item>
-          </el-form>
-        </el-card>
-      </div>
-
+      <el-row :gutter="20">
+<!--        xs	<768px 响应式栅格数或者栅格属性对象	用于超小型设备-->
+<!--        sm	≥768px 响应式栅格数或者栅格属性对象	用于小屏设备-->
+<!--        md	≥992px 响应式栅格数或者栅格属性对象	用于中屏设备-->
+<!--        lg	≥1200px 响应式栅格数或者栅格属性对象	用于大屏设备-->
+<!--        xl	≥1920px 响应式栅格数或者栅格属性对象	用于超大屏设备-->
+        <el-col :xs="24" :sm="26" :md="24" :lg="7" :xl="6">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span style="font-size: 30px">个人应用系统</span>
+              <el-button style="float: right;width: 150px" type="primary" @click="enterMyTools">点击进入</el-button>
+            </div>
+            <el-form style="background-image: url('../../static/img/img3.jpg');background-size: cover;height: 200px">
+              <el-form-item label="说明：" ></el-form-item>
+              <el-form-item>用于个人应用开发</el-form-item>
+              <el-form-item></el-form-item>
+            </el-form>
+          </el-card>
+        </el-col>
+        <el-col :xs="24" :sm="26" :md="24" :lg="7" :xl="6">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span style="font-size: 30px">后台管理系统</span>
+              <el-button style="float: right;width: 150px" type="primary" @click="enterMMS">点击进入</el-button>
+            </div>
+            <el-form style="background-image: url('../../static/img/img2.png');background-size: cover;height: 200px">
+              <el-form-item label="2022-02-07  更新说明：" ></el-form-item>
+              <el-form-item style="font-size: 200%">1，加入首页的卡片界面</el-form-item>
+              <el-form-item>1，更新首页信息</el-form-item>
+            </el-form>
+          </el-card>
+        </el-col>
+        <el-col :xs="24" :sm="26" :md="24" :lg="7" :xl="6">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span style="font-size: 30px">前端组件Demo库</span>
+              <el-button style="float: right;width: 150px" type="primary" @click="enterDemo">点击进入</el-button>
+            </div>
+            <el-form style="background-image: url('../../static/img/img4.png');background-size: cover;height: 200px">
+              <el-form-item label="说明：" ></el-form-item>
+              <el-form-item>用于记录前端的组件应用</el-form-item>
+              <el-form-item></el-form-item>
+            </el-form>
+          </el-card>
+        </el-col>
+        <el-col :xs="24" :sm="26" :md="24" :lg="7" :xl="6">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span style="font-size: 30px">开发中......</span>
+              <el-button style="float: right;width: 150px" type="primary" @click="enterTest">点击进入</el-button>
+            </div>
+            <el-form style="background-image: url('../../static/img/img1.jpg');background-size: cover;height: 200px">
+              <el-form-item label="说明：" ></el-form-item>
+              <el-form-item>通过路由控制系统入口，进行转发</el-form-item>
+              <el-form-item></el-form-item>
+            </el-form>
+          </el-card>
+        </el-col>
+        </el-row>
     </el-container>
   </div>
 </template>
